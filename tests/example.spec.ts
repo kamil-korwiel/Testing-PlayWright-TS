f728474f4ba2adf154a2547ae5bd2cf4ba4aa799
kamil-korwiel <kamilkorwiel@gmail.com>
2025-01-17 20:40:54 +0100

    feat: make a file that show a coverage at e2e

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,35 @@
+// @ts-check
 import { test, expect } from '@playwright/test';
+import MCR from "monocart-coverage-reports";
 
+test('has title', async ({ page }) => {
+  
+  await Promise.all([
+      page.coverage.startJSCoverage({
+          resetOnNavigation: false
+      }),
+      page.coverage.startCSSCoverage({
+          resetOnNavigation: false
+      })
+  ]);
 
-// Use test and expect as usual
-test("basic test", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  await expect(page).toHaveTitle("Vite + React + TS");
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+
+  const [jsCoverage, cssCoverage] = await Promise.all([
+      page.coverage.stopJSCoverage(),
+      page.coverage.stopCSSCoverage()
+  ]);
+
+  const coverageData = [... jsCoverage, ... cssCoverage];
+
+  const mcr = MCR({
+      name: 'My Coverage Report - 2024-02-28',
+      outputDir: './coverage-reports',
+      reports: ["v8", "console-details"],
+      cleanCache: true
+  });
+  await mcr.add(coverageData);
+  await mcr.generate();
+  
 });
